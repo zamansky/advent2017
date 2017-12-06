1232be9d498174466faba3c26bf2f6cf236a580b
Mike Zamansky <zamansky@gmail.com>
2017-12-06 08:56:53 -0500

    Finished day6

--- day6.py
+++ day6.py
@@ -1,2 +1,19 @@
-l = [ int(x) for x in open("d6.test").read().strip().split()]
+l = [ int(x) for x in open("d6.txt").read().strip().split()]
 print(l)
+d = {}
+steps = 0
+key = tuple(l)
+llen = len(l)
+while key not in d:
+    d[key]=steps
+    steps = steps + 1
+    biggest = max(l)
+    i = l.index(biggest)
+    l[i]=0
+    ti = i
+    for x in range(biggest):
+        ti = (ti + 1)%llen
+        l[ti] = l[ti] + 1
+    key = tuple(l)
+print(key,steps,steps - d[key])
+    
